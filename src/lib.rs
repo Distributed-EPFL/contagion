60d597053f705c2c67da520741eafbae5d01139f
Ogier Bouvier <ogier@bouvier.family>
2021-04-16 14:42:19 +0200

    remove stale non batched version
    Signed-off-by: Ogier Bouvier <ogier@bouvier.family>

--- src/lib.rs
+++ src/lib.rs
@@ -5,17 +5,6 @@
 //! broadcast, as it additionally guarantees e-totality.  Despite a Byzantine
 //! sender, either none or every correct process delivers the broadcasted message.
 //!
-//! [`Contagion`]: crate::classic::Contagion
-
-/// This module provides [`BatchedContagion`] which is an improved version of `Contagion` that supports multiple
-/// senders and provides optimizations for latency and reduced network IO.
-///
-/// [`BatchedContagion`]: crate::batched::BatchedContagion
-pub mod batched;
-
-/// This module provides the classic version of [`Contagion`] that only supports either sender or receiver mode and
-/// can only
-/// broadcast/receive one message per instance and produces a lot of unnecessary traffic
-///
-/// [`Contagion`]: crate::classic::Contagion
-pub mod classic;
+//! [`Contagion`]: crate::Contagion
+mod batched;
+pub use batched::*;
